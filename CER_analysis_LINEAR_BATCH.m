f867684aea9237874965a4aa21952fbe0803ecff
Naveen Sendhilnathan <59427583+naveen-7@users.noreply.github.com>
2020-10-12 15:11:18 -0400

    Update CER_analysis_LINEAR_BATCH.m

--- CER_analysis_LINEAR_BATCH.m
+++ CER_analysis_LINEAR_BATCH.m
@@ -10,39 +10,39 @@
 
 
  
-% % %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% % %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % FOR REGULAR, RELEASE HERE -----------------------------------------
 
-clc;
-clear ;
-close all;
+%clc;
+%clear ;
+%close all;
 
 
-codes_dir = fullfile('E:','NAVEEN_Work','Cerebellum','Codes','CER_codes_NEW','LINEAR');
-data_dir  = fullfile('E:','NAVEEN_Work','Cerebellum','Data','ALL_CELLS');
+% codes_dir = fullfile('E:','NAVEEN_Work','Cerebellum','Codes','CER_codes_NEW','LINEAR');
+% data_dir  = fullfile('E:','NAVEEN_Work','Cerebellum','Data','ALL_CELLS');
 
-disp('!!!!!  CER_analysis_LINEAR_BATCH has started running  !!!!!')
-cd(data_dir)
+% disp('!!!!!  CER_analysis_LINEAR_BATCH has started running  !!!!!')
+% cd(data_dir)
 
-% cd('E:\NAVEEN_Work\Cerebellum\Data\MERGED_CELLS');
+% % cd('E:\NAVEEN_Work\Cerebellum\Data\MERGED_CELLS');
 
-% % LOADING FILE--------------------------------------------------------
-disp('*************************************************')
-disp('*************************************************')
-disp('LOAD THE CELL DATA FILE')
-[FileName1,PathName1] = uigetfile('*.mat','File to Append');   % Open standard dialog box for retrieving files
-DATAfile = cat(2,PathName1,FileName1);
-disp(strcat('!!!!!','File you entered is :',FileName1,' !!!!!'));
-load(DATAfile);
-cd(PathName1);
+% % % LOADING FILE--------------------------------------------------------
+% disp('*************************************************')
+% disp('*************************************************')
+% disp('LOAD THE CELL DATA FILE')
+% [FileName1,PathName1] = uigetfile('*.mat','File to Append');   % Open standard dialog box for retrieving files
+% DATAfile = cat(2,PathName1,FileName1);
+% disp(strcat('!!!!!','File you entered is :',FileName1,' !!!!!'));
+% load(DATAfile);
+% cd(PathName1);
 
 
 % % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % % % % % FOR BATCH, RELEASE HERE -----------------------------------------
 % % 
-% % close all;
-% % File = DATAfile;
-% % FileName = Tempy;
+close all;
+File = DATAfile;
+FileName = Tempy;
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
