f7035cb4a306c0ccb0f7b31acc0f4847c06fb678
naveeen7 <59427583+naveen-7@users.noreply.github.com>
2021-09-25 14:35:37 -0700

    Create Prelim_ADAPTEDL_n.m

---
+++ Codes/Prelim_ADAPTEDL_n.m
@@ -0,0 +1,13 @@
+%% Makes the cell plate and gives all necessary information about the code
+% Created by NAVEEN ON 8/11/17 at CUMC
+
+
+
+function LICK_SIGNAL = Prelim_ADAPTEDL_n(LICK_DATA_IMPORT,Trials,Trigg)
+
+
+CER_analysis_01_LICK2
+
+disp('!!! END OF CODE Prelim_ADAPTEDL_n !!!');
+
+end
